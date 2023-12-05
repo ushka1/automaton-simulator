4ec370e69f6d7e02ca68d81482025d38cde4316f
ushka1 <dawid.prajzner01@gmail.com>
2023-12-05 18:36:24 +0100

    add curved transition

--- src/render/TransitionView.ts
+++ src/render/TransitionView.ts
@@ -1,20 +1,23 @@
 import { StateView } from './StateView';
 
 export class TransitionView {
-  private line: SVGLineElement;
+  private path: SVGPathElement;
   private startStateView?: StateView;
   private startMountPointIndex?: number;
   private endStateView?: StateView;
   private endMountPointIndex?: number;
 
   constructor() {
-    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    line.style.stroke = 'var(--blue)';
-    line.style.strokeWidth = '5';
-    line.style.pointerEvents = 'none';
-    line.setAttribute('marker-end', 'url(#arrow)');
-    this.line = line;
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.style.stroke = 'var(--blue)';
+    path.style.strokeWidth = '5';
+    path.style.pointerEvents = 'none';
+    path.style.fill = 'none';
+    path.setAttribute('marker-end', 'url(#arrow)');
+    this.path = path;
   }
+
+  // ========================= OBSERVE STATES =========================
 
   setStartStateView(stateView: StateView, mountPointIndex: number) {
     this.unsetStartStateView();
@@ -64,17 +67,71 @@ export class TransitionView {
     this.updateEnd(coords);
   };
 
+  /* ========================= UPDATE PATH ========================= */
+
   updateStart = (coords: { x: number; y: number }) => {
-    this.line.setAttribute('x1', `${coords.x}`);
-    this.line.setAttribute('y1', `${coords.y}`);
+    const endCoords = this.getEndCoords();
+    const pathData = this.getPathData(coords, endCoords);
+    this.path.setAttribute('d', pathData);
   };
 
   updateEnd = (coords: { x: number; y: number }) => {
-    this.line.setAttribute('x2', `${coords.x}`);
-    this.line.setAttribute('y2', `${coords.y}`);
+    const startCoords = this.getStartCoords();
+    const pathData = this.getPathData(startCoords, coords);
+    this.path.setAttribute('d', pathData);
   };
 
-  getSvg(): SVGLineElement {
-    return this.line;
+  private getStartCoords(): { x: number; y: number } {
+    const pathSegments = this.path.getAttribute('d')?.split(' ') || [];
+    const x = parseFloat(pathSegments[0].substring(1));
+    const y = parseFloat(pathSegments[1]);
+
+    if (!isNaN(x) && !isNaN(y)) {
+      return { x, y };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+
+  private getEndCoords(): { x: number; y: number } {
+    const pathSegments = this.path.getAttribute('d')?.split(' ') || [];
+    const x = parseFloat(pathSegments[4]);
+    const y = parseFloat(pathSegments[5]);
+
+    if (!isNaN(x) && !isNaN(y)) {
+      return { x, y };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+
+  private getPathData(
+    startCoords: { x: number; y: number },
+    endCoords: { x: number; y: number },
+  ): string {
+    const curvatureOffset = 0;
+    const controlX = (startCoords.x + endCoords.x) / 2;
+    const controlY = (startCoords.y + endCoords.y) / 2 - curvatureOffset;
+
+    return (
+      'M' +
+      startCoords.x +
+      ' ' +
+      startCoords.y +
+      ' Q' +
+      controlX +
+      ' ' +
+      controlY +
+      ' ' +
+      endCoords.x +
+      ' ' +
+      endCoords.y
+    );
+  }
+
+  /* ========================= UTILS ========================= */
+
+  getSvg(): SVGPathElement {
+    return this.path;
   }
 }
