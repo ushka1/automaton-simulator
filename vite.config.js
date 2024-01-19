850d0622c9f1d76997d8975db52028299fcdeaf3
ushka1 <david.prajzner01@gmail.com>
2024-01-19 23:05:41 +0100

    fix html plugin

--- vite.config.js
+++ vite.config.js
@@ -12,55 +12,68 @@ const __dirname = path.dirname(__filename);
  *
  * This plugin searches for HTML comments of the form <!-- @include 'path/to/filename.ext' -->
  * in the index.html file and replaces them with the content of the specified file.
- *
- * @type {import('vite').Plugin}
  */
-const importHtmlPartialsPlugin = {
-  name: 'import-html-partials',
-  transformIndexHtml(html, ctx) {
-    let transformedHtml = html;
+const importHtmlPartialsPlugin = () => {
+  /**
+   * @type {import('vite').ResolvedConfig}
+   */
+  let config;
 
-    const regex = /<!-- @include '(.+)' -->/;
-    const globalRegex = /<!-- @include '(.+)' -->/g;
+  /**
+   * @type {import('vite').Plugin}
+   */
+  const plugin = {
+    name: 'import-html-partials',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+    transformIndexHtml(html) {
+      let transformedHtml = html;
 
-    const matches = transformedHtml.match(globalRegex);
-    if (!matches) {
-      return transformedHtml;
-    }
+      const regex = /<!-- @include '(.+)' -->/;
+      const globalRegex = /<!-- @include '(.+)' -->/g;
 
-    for (const match of matches) {
-      const filePath = match.match(regex)[1];
-      let absolutePath;
+      const matches = transformedHtml.match(globalRegex);
+      if (!matches) {
+        return transformedHtml;
+      }
 
-      // Check if the file path matches any of the aliases defined in vite.config.js.
-      const aliases = ctx.server.config.resolve.alias;
-      for (const alias in aliases) {
-        const { find, replacement } = aliases[alias];
-        if (filePath.match(find)) {
-          absolutePath = filePath.replace(find, replacement);
+      for (const match of matches) {
+        const filePath = match.match(regex)[1];
+        let absolutePath;
+
+        // Check if the file path matches any of the aliases defined in vite.config.js.
+        const aliases = config.resolve.alias;
+        for (const alias in aliases) {
+          const { find, replacement } = aliases[alias];
+          if (filePath.match(find)) {
+            absolutePath = filePath.replace(find, replacement);
+          }
+        }
+
+        // If the file path is not an alias, resolve it relative to the vite.config.js file.
+        if (!absolutePath) {
+          absolutePath = path.resolve(__dirname, filePath);
+        }
+
+        // Read the file content and replace the HTML comment with it.
+        try {
+          const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+          transformedHtml = transformedHtml.replace(match, fileContent);
+        } catch (error) {
+          console.error(`Error importing file ${filePath}: ${error.message}`);
         }
       }
 
-      // If the file path is not an alias, resolve it relative to the vite.config.js file.
-      if (!absolutePath) {
-        absolutePath = path.resolve(__dirname, filePath);
-      }
+      return transformedHtml;
+    },
+  };
 
-      // Read the file content and replace the HTML comment with it.
-      try {
-        const fileContent = fs.readFileSync(absolutePath, 'utf-8');
-        transformedHtml = transformedHtml.replace(match, fileContent);
-      } catch (error) {
-        console.error(`Error importing file ${filePath}: ${error.message}`);
-      }
-    }
-
-    return transformedHtml;
-  },
+  return plugin;
 };
 
 export default defineConfig({
-  plugins: [eslint(), importHtmlPartialsPlugin],
+  plugins: [eslint(), importHtmlPartialsPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
